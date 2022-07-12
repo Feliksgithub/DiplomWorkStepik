f0abb4a1f5f36f81f2ca6a2c17db4691be883a08
Feliks <bautr@yandex.ru>
2022-07-12 17:02:56 +0300

    add_book_in_basket

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") 
     REGISTER_FORM =(By.CSS_SELECTOR, "#register_form") 
+
+class BasketPageLocators(object):
+    ADD_BASKET = (By.XPATH, "//*[@id='add_to_basket_form']/button")
+    ADD_MESSAGE = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    BOOK_NAME = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/h1")
+    PRICE_BASKET = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
+    PRICE_BOOK = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/p[1]")
