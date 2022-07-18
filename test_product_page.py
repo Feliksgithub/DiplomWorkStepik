d29cf8319c35ae8388aec818dd21efd860d8c80c
Feliks <bautr@yandex.ru>
2022-07-18 10:40:45 +0300

    END

--- test_product_page.py
+++ test_product_page.py
@@ -4,18 +4,8 @@ from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 
-@pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link, 0)
-    page.open()
-    page.add_book_in_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_add_message()
-    page.should_be_same_price_basket()   
-    
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket_promo(browser, link):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, link)
     page.open()
@@ -93,3 +83,13 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.add_book_in_basket()
         page.should_be_add_message()   
+        
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.add_book_in_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_add_message()
+    page.should_be_same_price_basket()  
